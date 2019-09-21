2f0b7536c7e7d7330de057ed9d33546fe04f0af7
Shane <shanemcneil19@gmail.com>
2019-09-21 15:01:47 -0700

    create component folders and complete App file

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
-import AddBookmark from './components/AddBookmark'
+import AddBookmark from './AddBookmark/AddBookmark';
+import BookmarkApp from './BookmarkApp/BookmarkApp';
 
-class App extends Component {
+
+const bookmarks = [
+  {
+  title:"Google",
+  url:"http://www.google.com", 
+  rating:"3", 
+  description:"No evil"
+  },
+  {
+    title:"Google",
+    url:"http://www.google.com", 
+    rating:"3", 
+    description:"No evil"
+  }
+];
+
+export default class App extends Component {
   render() {
     return (
       <div className="App">
+        <AddBookmark />
+        <BookmarkApp bookmarks={bookmarks}/>
+        
       </div>
     );
   }
 }
-
-export default App;
